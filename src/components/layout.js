eed1bdba770dd0371be28b44b9bae98f9f264069
MasonJArnold <mason.jarnold@gmail.com>
2020-07-17 18:17:01 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,7 +15,7 @@ import MainMenu from './MainMenu'
 import styled, {createGlobalStyle} from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap');
+
 
   body{
     font-family: 'Open Sans', sans-serif;
