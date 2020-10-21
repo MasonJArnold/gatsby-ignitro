d39142e5640ba394448373c99cd19b0f2629af65
Joseph Gross <joseph@ignitro.com>
2020-10-21 13:09:38 -0400

    merge

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  plugins: [`gatsby-plugin-styled-components`],
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -26,6 +26,18 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-source-gravityforms',
+      options: {
+        // Base URL needs to include protocol (http/https)
+        baseUrl: 'https://gatsbyreact.wpengine.com',
+        // Gravity Forms API
+        api: {
+          key: process.env.GF_CONSUMER_KEY,
+          secret: process.env.GF_CONSUMER_SECRET,
+        },
       },
     },
   //   {
@@ -72,7 +84,7 @@ module.exports = {
       verboseOutput: true,
       searchAndReplaceContentUrls: {
         sourceUrl: "inspiring-meninsky-da9386.netlify.app",
-        replacementUrl: "gatsbyreact.wpengine.com/",
+        replacementUrl: "gatsbyreact.wpengine.com",
       },
     },
   },
